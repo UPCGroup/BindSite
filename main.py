ae12d63dd0c9dc0fd4448c1d478a186c3b1a5d6e
1i9h7_b1u3 <1012796366@qq.com>
2023-07-22 10:51:44 +0800

    字符索引问题修复

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ def convert(fileName):
     df = pd.read_csv(fileName, sep="|")
     target = pd.DataFrame(columns=["SequenceID", "Site", "Label"])
     for _,item in df.iterrows():
+        count = 0
         for index, curr_char in enumerate(item["Label"]):
             if curr_char == '#':
-                target.loc[len(target)] = [item["SequenceID"], index, item["Label"].replace('#', '')]
+                # 注意 DeepPhos 的字符索引从 1 开始
+                # 因此在字符串中索引为 0 的 S T 或者 Y，其位点应当为 1
+                target.loc[len(target)] = [item["SequenceID"], index + count, item["Label"].replace('#', '')]
+                count -= 1
 
     if os.path.exists("./output/" + fileName) and os.path.isfile("./output/" + fileName):
         os.remove("./output/" + fileName)
